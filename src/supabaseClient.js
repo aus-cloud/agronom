6a76ed42a7a29552599d723845148d6b69404c53
Ismingiz~ <sirojov0660@gmail.com>
2026-01-18 14:00:54 +0500

    Realtime ва статус ўзгаришлари қўшилди

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,4 +3,10 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://ufgadyosoeprynwdtahz.supabase.co'
 const supabaseAnonKey = 'sb_publishable__cIoXi8c2SfJoaAskWHqNg_o-uphJUP'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  realtime: {
+    params: {
+      eventsPerSecond: 10,
+    },
+  },
+})
